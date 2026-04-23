c1b9d5f14d03fa7212271e68e38bab8f6d673edf
StephenCotterrell <stephen@cotterrell.dev>
2026-04-22 17:03:57 -0700

    allow navigation within the selection

--- cmd/internal/app/model.go
+++ cmd/internal/app/model.go
@@ -37,6 +37,21 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.Profiles) {
+				m.cursor++
+			}
+		case "enter", "space":
+			_, ok := m.Selected[m.cursor]
+			if ok {
+				delete(m.Selected, m.cursor)
+			} else {
+				m.Selected[m.cursor] = struct{}{}
+			}
 		}
 	}
 
@@ -65,6 +80,8 @@ func (m Model) View() tea.View {
 
 		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, profile)
 	}
+
+	s += "\nPress q to quit."
 
 	return tea.NewView(s)
 }
